5d4429bf01191e2ce1b6a6714f3fd8dd5515bb0f
K31THSTONE <goliff@ycp.edu>
2017-10-25 13:23:06 -0400

    Started putting skeleton code on main.c

--- main.c
+++ main.c
@@ -20,20 +20,60 @@ int main(int argc, char** argv)
     
     
     // TODO: parse arguments
-    
+    int aflag = 0;
+    int bflag = 0;
+    char *cvalue = NULL;
+    int index;
+    int c;
+
+    opterr = 0;
+
+
+    while ((c = getopt (argc, argv, "abc:")) != -1)
+      switch (c)
+        {
+        case 'a':
+          aflag = 1;
+          break;
+        case 'b':
+          bflag = 1;
+          break;
+        case 'c':
+          cvalue = optarg;
+          break;
+        case '?':
+          if (optopt == 'c')
+            fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+          else if (isprint (optopt))
+            fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+          else
+            fprintf (stderr,
+                     "Unknown option character `\\x%x'.\n",
+                     optopt);
+          return 1;
+        default:
+          abort ();
+        }
+
     
     
     // TODO: write initial '0' to file
-    
+    open_file();
+    //write 0
+    close_file();
     
 
     // TODO: create a named semaphore
-    
+    //sem_t *sem_open(const char *name, int oflag, mode_t mode, int value);
     
     
     // TODO: fork off child processes and wait for them to finish
-
-    
+    if(fork() == 0){
+    }
+    else{
+      sem_wait();
+    }
+ 
     
     // TODO: clean up and close named semaphore
     
